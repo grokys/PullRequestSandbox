38ed537dea2bcb009fcdd333d36d22b442277fcc
Steven Kirk <grokys@users.noreply.github.com>
2016-11-30 23:48:41 +0100

    Update Program.cs

--- PullRequestSandbox/Program.cs
+++ PullRequestSandbox/Program.cs
@@ -11,12 +11,6 @@ namespace PullRequestSandbox
         static void Main(string[] args)
         {
             // Testing GHfVS behind state.
-            // Change 1.
-            // Change 2.
-            // Change 3.
-            // Change 4.
-            // Change 5.
-            // Change 6.
         }
     }
 }
