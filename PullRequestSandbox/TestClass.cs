ea344016dc246e1fd2851a3295eea6f689525a7c
Steven Kirk <grokys@gmail.com>
2017-04-20 20:47:45 +0200

    Added test class for review comments.

---
+++ PullRequestSandbox/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InlineComments
+{
+    class TestClass
+    {
+        public int PropertyWithComment1 { get; set; }
+        public int Property1 { get; }
+        public int Property2 { get; }
+        public int PropertyWithComment2 { get; set; }
+
+        public void Method1()
+        {
+            // Method1 method contents.
+        }
+
+        public void MethodWithComment2()
+        {
+            // MethodWithComment2 method contents.
+        }
+
+        public void Method3()
+        {
+            // Method1 method contents.
+        }
+    }
+}
