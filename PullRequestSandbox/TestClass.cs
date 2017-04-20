3398937ee48804e55834cb0c4174c6c0832d580f
Steven Kirk <grokys@gmail.com>
2017-04-20 20:50:00 +0200

    Made some changes.

--- PullRequestSandbox/TestClass.cs
+++ PullRequestSandbox/TestClass.cs
@@ -6,20 +6,15 @@ namespace InlineComments
     {
         public int PropertyWithComment1 { get; set; }
         public int Property1 { get; }
-        public int Property2 { get; }
         public int PropertyWithComment2 { get; set; }
-
-        public void Method1()
-        {
-            // Method1 method contents.
-        }
+        public int Property2 { get; }
 
         public void MethodWithComment2()
         {
             // MethodWithComment2 method contents.
         }
 
-        public void Method3()
+        public void Method1()
         {
             // Method1 method contents.
         }
