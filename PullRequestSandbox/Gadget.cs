d4f8cf6e06f260ebbb9244e3f1ad8497c35ade63
Steven Kirk <grokys@gmail.com>
2017-07-13 14:03:30 +0200

    Added gadget

---
+++ PullRequestSandbox/Gadget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PullRequestSandbox
+{
+    class Gadget
+    {
+        Dictionary<string, string> values = new Dictionary<string, string>();
+
+        public void SetBlerp(string value)
+        {
+            Guard.NotNull(value, nameof(value));
+
+            values["blerp"] = value;
+        }
+
+        public void SetBloop(string value)
+        {
+            Guard.NotNull(value, nameof(value));
+
+            values["bloop"] = value;
+        }
+    }
+}
