743756ca387fc70df229aed26e0ea3b069bdcdbb
Jamie Cansdale <jcansdale@gmail.com>
2017-10-12 09:28:04 +0100

    Include "foo\r\rbar" in file

--- PullRequestSandbox/Program.cs
+++ PullRequestSandbox/Program.cs
@@ -5,6 +5,7 @@
 
 using System; // comment on first line
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace PullRequestSandbox
     {
         static void Main(string[] args)
         {
+            File.WriteAllText(@"..\..\extra_carriage_return.txt", "foo\r\rbar");
         }
     }
 }
